62c3778bdb2479380c1d6bc6d53401e5696cd751
go <go.nobpadon25@gmail.com>
2025-03-18 17:12:30 +0700

    refactor: DTO Pagination

--- ThailandpostTracking_api/DTOs/Thailandpost/Request/GetTrackingDetailRequestDTO.cs
+++ ThailandpostTracking_api/DTOs/Thailandpost/Request/GetTrackingDetailRequestDTO.cs
@@ -2,15 +2,12 @@
 
 namespace ThailandpostTracking.DTOs.Thailandpost.Request
 {
-    public class GetTrackingDetailRequestDTO : PaginationDto
+    public class GetTrackingDetailRequestDTO
     {
         [Required]
         public Guid TrackingHeaderId { get; set; }
 
         [Required]
         public Guid TrackingBatchId { get; set; }
-
-        public string? OrderingField { get; set; }
-        public bool AscendingOrder { get; set; } = true;
     }
 }
