8516ab1bc4edf40ead83db11f1c9abad0aff6caa
go <go.nobpadon25@gmail.com>
2025-03-19 09:22:41 +0700

    refactor: setting add take

--- ThailandpostTracking_api/Configurations/ThailandpostTrackingSetting.cs
+++ ThailandpostTracking_api/Configurations/ThailandpostTrackingSetting.cs
@@ -5,5 +5,7 @@
         public string APIKey { get; set; }
         public string GetTokenEnpoint { get; set; }
         public string GetItemsbyBarcodeEnpoint { get; set; }
+        public int TakeInSert { get; set; }
+        public int TakeUpSert { get; set; }
     }
 }
