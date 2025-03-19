b31e600e1e9d79341819ba7691b276b5b0255045
go <go.nobpadon25@gmail.com>
2025-03-20 01:16:04 +0700

    Refactor DTOs for improved organization and clarity
    Restructured `GetTimeLineResponseDTO` by reordering properties, removing and re-adding `TrackingDetails` for better clarity.
    
    In `GetTrackingHeaderResponseDTO`, removed several properties to simplify the class structure, but later re-evaluated and re-added `TmpImportTrackingId`, `TmpImportTracking`, and `TrackingBatch`. Introduced a new nullable `TrackingBatchId` property.
    
    These changes enhance the maintainability and clarity of the DTOs in the Thailandpost tracking system.

--- ThailandpostTracking_api/DTOs/Thailandpost/Response/GetTimeLineResponseDTO.cs
+++ ThailandpostTracking_api/DTOs/Thailandpost/Response/GetTimeLineResponseDTO.cs
@@ -4,11 +4,10 @@ namespace ThailandpostTracking.DTOs.Thailandpost.Response
 {
     public class GetTimeLineResponseDTO
     {
-        public Guid TrackingBatchId { get; set; }
-        public GetTrackingBatchResponseDTO TrackingBatch { get; set; }
+        public List<GetTrackingDetailResponseDTO> TrackingDetails { get; set; }
         public Guid TmpImportTrackingId { get; set; }
         public GetTmpImportTrackingResponseDTO TmpImportTracking { get; set; }
-
-        public List<GetTrackingDetailResponseDTO> TrackingDetails { get; set; }
+        public Guid TrackingBatchId { get; set; }
+        public GetTrackingBatchResponseDTO TrackingBatch { get; set; }
     }
 }
--- ThailandpostTracking_api/DTOs/Thailandpost/Response/GetTrackingHeaderResponseDTO.cs
+++ ThailandpostTracking_api/DTOs/Thailandpost/Response/GetTrackingHeaderResponseDTO.cs
@@ -7,10 +7,6 @@ namespace ThailandpostTracking.DTOs.Thailandpost.Response
     public class GetTrackingHeaderResponseDTO
     {
         public Guid TrackingHeaderId { get; set; }
-        public GetTrackingBatchResponseDTO TrackingBatch { get; set; }
-        public Guid TmpImportTrackingId { get; set; }
-        public GetTmpImportTrackingResponseDTO TmpImportTracking { get; set; }
-
         public string TrackingCode { get; set; }
 
         public string ReferenceCode { get; set; }
@@ -42,5 +38,9 @@ namespace ThailandpostTracking.DTOs.Thailandpost.Response
         public string Office_Tel { get; set; }
 
         public string Call_Center_Tel { get; set; }
+        public Guid TmpImportTrackingId { get; set; }
+        public GetTmpImportTrackingResponseDTO TmpImportTracking { get; set; }
+        public Guid? TrackingBatchId { get; set; }
+        public GetTrackingBatchResponseDTO TrackingBatch { get; set; }
     }
 }
