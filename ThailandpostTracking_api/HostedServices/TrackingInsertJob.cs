e85827be4f513b1736eec5c1c56fd57c3b8a3e3e
go <go.nobpadon25@gmail.com>
2025-03-19 09:23:23 +0700

    refactor:  add take

--- ThailandpostTracking_api/HostedServices/TrackingInsertJob.cs
+++ ThailandpostTracking_api/HostedServices/TrackingInsertJob.cs
@@ -1,5 +1,7 @@
-﻿using Quartz;
+﻿using Microsoft.Extensions.Options;
+using Quartz;
 using Serilog;
+using ThailandpostTracking.Configurations;
 using ThailandpostTracking.Data;
 using ThailandpostTracking.DTOs.Thailandpost.Request;
 using ThailandpostTracking.Services.ThailandpostTracking;
@@ -10,16 +12,18 @@ namespace ThailandpostTracking.HostedServices
     {
         private readonly IThailandpostTrackingServices _services;
         private readonly AppDBContext _dbContext;
+        private readonly ThailandpostTrackingSetting _configuration;
 
-        public TrackingInsertJob(IThailandpostTrackingServices services, AppDBContext dbContext)
+        public TrackingInsertJob(IThailandpostTrackingServices services, AppDBContext dbContext, IOptions<ThailandpostTrackingSetting> configuration)
         {
             _services = services;
             _dbContext = dbContext;
+            _configuration = configuration.Value;
         }
 
         public async Task Execute(IJobExecutionContext context)
         {
-            var tmpImportTrackings = _dbContext.TmpImportTrackings.Where(x => x.IsInsert == null && x.IsResult == null).OrderBy(a => a.TmpImportTrackingId).Take(100).ToList();
+            var tmpImportTrackings = _dbContext.TmpImportTrackings.Where(x => x.IsInsert == null && x.IsResult == null).OrderBy(a => a.TmpImportTrackingId).Take(_configuration.TakeInSert).ToList();
             var input = new GetItemsbyBarcodeRequestDTO
             {
                 Status = "all",
--- ThailandpostTracking_api/HostedServices/TrackingUpsertJob.cs
+++ ThailandpostTracking_api/HostedServices/TrackingUpsertJob.cs
@@ -1,5 +1,7 @@
-﻿using Quartz;
+﻿using Microsoft.Extensions.Options;
+using Quartz;
 using Serilog;
+using ThailandpostTracking.Configurations;
 using ThailandpostTracking.Data;
 using ThailandpostTracking.DTOs.Thailandpost.Request;
 using ThailandpostTracking.Services.ThailandpostTracking;
@@ -10,16 +12,18 @@ namespace ThailandpostTracking.HostedServices
     {
         private readonly IThailandpostTrackingServices _services;
         private readonly AppDBContext _dbContext;
+        private readonly ThailandpostTrackingSetting _configuration;
 
-        public TrackingUpsertJob(IThailandpostTrackingServices services, AppDBContext dbContext)
+        public TrackingUpsertJob(IThailandpostTrackingServices services, AppDBContext dbContext, IOptions<ThailandpostTrackingSetting> configuration)
         {
             _services = services;
             _dbContext = dbContext;
+            _configuration = configuration.Value;
         }
 
         public async Task Execute(IJobExecutionContext context)
         {
-            var tmpImportTrackings = _dbContext.TrackingHeaders.Where(x => x.IsActive == true).OrderBy(a => a.UpdatedDate).Take(10).ToList();
+            var tmpImportTrackings = _dbContext.TrackingHeaders.Where(x => x.IsActive == true && x.Status != "501").OrderBy(a => a.UpdatedDate).Take(_configuration.TakeUpSert).ToList();
             var input = new GetItemsbyBarcodeRequestDTO
             {
                 Status = "all",
